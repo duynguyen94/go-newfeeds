9f1014bae1450a2639a8996d97477cafbe5578c8
Duy Nguyen <duy.nguyen@inkr.com>
2024-08-13 10:45:44 +0700

    init users-api

--- cmd/users/main.go
+++ cmd/users/main.go
@@ -1,1 +1,38 @@
-package users
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/health-check", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "OK",
+		})
+	})
+
+	// TODO Login
+	r.POST("/v1/users/login", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Need implementation",
+		})
+	})
+
+	// TODO SignUp
+	r.POST("/v1/users", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Need implementation",
+		})
+	})
+
+	// TODO Edit profile
+	r.PUT("/v1/users", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Need implementation",
+		})
+	})
+
+	r.Run()
+}
