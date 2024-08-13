c42d688e38bfc379c258958a705cb62f8b48b94a
Duy Nguyen <duy.nguyen@inkr.com>
2024-08-13 12:27:44 +0700

    add hashed pass

--- cmd/users/models.go
+++ cmd/users/models.go
@@ -5,15 +5,18 @@ import (
 )
 
 const DOBLayout = "2006-01-02"
+const saltSize = 16
 
 type UserRecord struct {
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Email     string `json:"email"`
-	UserName  string `json:"userName"`
-	Password  string `json:"password"`
-	DOB       string `json:"dob"`
-	DOBDate   time.Time
+	FirstName  string `json:"firstName"`
+	LastName   string `json:"lastName"`
+	Email      string `json:"email"`
+	UserName   string `json:"userName"`
+	Password   string `json:"password"`
+	DOB        string `json:"dob"`
+	DOBDate    time.Time
+	salt       string
+	hashedPass string
 }
 
 func (u *UserRecord) DOBtoDate() error {
@@ -24,4 +27,12 @@ func (u *UserRecord) DOBtoDate() error {
 
 	u.DOBDate = t
 	return nil
+}
+
+func (u *UserRecord) HashPassword() {
+	salt := genRandomSalt(saltSize)
+	hashedPass := hashPassword(u.Password, salt)
+
+	u.salt = string(salt[:])
+	u.hashedPass = hashedPass
 }
